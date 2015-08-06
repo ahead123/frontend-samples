cf4acdce56b3a7f8c3f2ddc99aa612c5b59482a9
Arthur Head <ahead747@gmail.com>
2015-08-06 17:19:58 -0500

    finish quiz-3 & tests pass

--- quiz-3/quiz-3.js
+++ quiz-3/quiz-3.js
@@ -1,6 +1,16 @@
 
 function sortWords(sentence) {
 
+	// split input sentence into an array
+	var splitSentence = sentence.split(" ");
+
+	// compare the length of each string in the array and sort from low to high
+	var sortedSentence = splitSentence.sort(function(a,b){
+		return a.length - b.length
+	});
+
+	// join sentence back to string
+	return sortedSentence.join(" ")
 }
 
 console.log(sortWords("The quick brown fox jumped over the lazy dog"));
