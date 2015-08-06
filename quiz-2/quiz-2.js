674711339d9f9b623eddc6c3a660e108391358f1
Arthur Head <ahead747@gmail.com>
2015-08-06 16:35:10 -0500

    finish quiz 2 & tests pass

--- quiz-2/quiz-2.js
+++ quiz-2/quiz-2.js
@@ -4,9 +4,9 @@ function User(name) {
 }
 
 // Write your code here:
-// ---------------------
-
-// ---------------------
+User.prototype.sayName = function(){
+	console.log("Hello, my name is",this.name+"!");
+}
 
 var user1 = new User("Harry");
 user1.sayName();
